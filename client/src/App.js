9bbbcfcd3d5a4f1a39d0f80c47fdcc0aecd5133f
pizzaYami <cyj9407@naver.com>
2023-04-17 14:44:10 +0900

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyles = createGlobalStyle`
+  ${reset}
+    body {
+        background-color: white;
+    }
+`;
+
+function App() {
+  return (
+    <div>
+      <GlobalStyles />
+    </div>
+  );
+}
+
+export default App;
