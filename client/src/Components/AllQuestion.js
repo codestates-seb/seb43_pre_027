210c3949600d84a70bd4013641d5972bc1d92e2f
pizzaYami <cyj9407@naver.com>
2023-04-18 21:13:55 +0900

    redux세팅

--- client/src/Components/AllQuestion.js
+++ client/src/Components/AllQuestion.js
@@ -1,26 +1,10 @@
 import styled from 'styled-components';
+import { useSelector } from 'react-redux';
 
 function AllQuestion() {
-  let 임시질문자료 = [
-    {
-      id: 0,
-      title: '아무것도 모르겠어요!',
-      body: '코딩을 하는데 발전이 없는거같습니다...',
-      userName: '조용주',
-    },
-    {
-      id: 1,
-      title: '모든걸알겠어요!',
-      body: '코딩을 하는데 발전이 많이 많이 있는거 같습니다...',
-      userName: '강동욱',
-    },
-    {
-      id: 2,
-      title: '내가 왜해야하죠!',
-      body: '요즘 MZ들은 말이야',
-      userName: '정유주',
-    },
-  ];
+  let 임시질문자료 = useSelector((state) => {
+    return state.임시질문자료;
+  });
 
   return (
     <div>
--- client/src/store.js
+++ client/src/store.js
@@ -1,5 +1,31 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+
+let 임시질문자료 = createSlice({
+  name: '임시질문자료',
+  initialState: [
+    {
+      id: 0,
+      title: '아무것도 모르겠어요!',
+      body: '코딩을 하는데 발전이 없는거같습니다...',
+      userName: '조용주',
+    },
+    {
+      id: 1,
+      title: '모든걸알겠어요!',
+      body: '코딩을 하는데 발전이 많이 많이 있는거 같습니다...',
+      userName: '강동욱',
+    },
+    {
+      id: 2,
+      title: '내가 왜해야하죠!',
+      body: '요즘 MZ들은 말이야',
+      userName: '정유주',
+    },
+  ],
+});
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    임시질문자료: 임시질문자료.reducer,
+  },
 });
