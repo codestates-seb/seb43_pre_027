5643a3f68df892e53a213d55045c058527af0a72
dobbykong <120040832+dobbykong@users.noreply.github.com>
2023-04-27 14:57:36 +0900

    좋아요/싫어요 버튼 기능 수정

--- client/src/Components/Questions/QuestionsBody.js
+++ client/src/Components/Questions/QuestionsBody.js
@@ -97,7 +97,7 @@ function QuestionsBody({ body }) {
               width="36"
               height="36"
               viewBox="0 0 36 36"
-              fill={totalVotes > 0 ? '#FFB41E' : '#BABFC3'}
+              fill={totalScore > 0 ? '#FFB41E' : '#BABFC3'}
             >
               <path d="M2 25h32L18 9 2 25Z"></path>
             </svg>
@@ -112,7 +112,7 @@ function QuestionsBody({ body }) {
               width="36"
               height="36"
               viewBox="0 0 36 36"
-              fill={totalVotes < 0 ? '#FF4F4F' : '#BABFC3'}
+              fill={totalScore < 0 ? '#FFB41E' : '#BABFC3'}
             >
               <path d="M2 11h32L18 27 2 11Z"></path>
             </svg>
