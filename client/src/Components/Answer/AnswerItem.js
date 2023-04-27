1e73ff1d0ce1f43ba0f5afed607be788da304209
dobbykong <120040832+dobbykong@users.noreply.github.com>
2023-04-27 14:56:36 +0900

    좋아요/싫어요 버튼 수정

--- client/src/Components/Answer/AnswerItem.js
+++ client/src/Components/Answer/AnswerItem.js
@@ -226,7 +226,7 @@ function AnswerItem({ answerItem, addAnswer, setAddAnswer }) {
               width="36"
               height="36"
               viewBox="0 0 36 36"
-              fill={totalVotes > 0 ? '#FFB41E' : '#BABFC3'}
+              fill={totalScore > 0 ? '#FFB41E' : '#BABFC3'}
             >
               <path d="M2 25h32L18 9 2 25Z"></path>
             </svg>
@@ -241,7 +241,7 @@ function AnswerItem({ answerItem, addAnswer, setAddAnswer }) {
               width="36"
               height="36"
               viewBox="0 0 36 36"
-              fill={totalVotes < 0 ? '#FF4F4F' : '#BABFC3'}
+              fill={totalScore < 0 ? '#FFB41E' : '#BABFC3'}
             >
               <path d="M2 11h32L18 27 2 11Z"></path>
             </svg>
